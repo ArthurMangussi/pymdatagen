be92ae2b32937e0cf8fda501ea41842acb4abe00
unknown <mangussiarthur@gmail.com>
2023-12-19 17:58:34 -0300

    mdatagen version 1.0.0

---
+++ mdatagen/__init__.py
@@ -0,0 +1,2 @@
+from mdatagen.multivariate import MNAR, MAR, MCAR
+from mdatagen.univariate import MNAR, MAR, MCAR
--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name="mdgen",
+    name="mdatagen",
     version="1.0.0",
-    packages=["mdgen"],
+    packages=["mdatagen"],
     license="MIT",
     author="Arthur Dantas Mangussi",
     author_email="mangussiarthur@gmail.com",
-    description="mdgen: Missing Data Generator",
+    url= "https://github.com/ArthurMangussi/mdatagen",
+    keywords=["machine learning", "preprocessing data"],
+    description="mdatagen: A Python library to Generate Artifical Missing Data",
     requires=["python >= 3.11",
               "numpy >= 1.25.0",
               "pandas >= 2.0.3",
-              "scikit-learn == 1.3.0"]
+              "scikit-learn == 1.3.0"],
+    download_url="https://github.com/ArthurMangussi/mdatagen/archive/refs/tags/v1.0.0.tar.gz",
+    classifiers=[
+    'Development Status :: 4 - Alpha',      
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.11'],
+    
 )
