b9b2d334ff799f4c0ff7aca7a8f68778dfc5cece
unknown <mangussiarthur@gmail.com>
2023-12-25 19:51:40 -0300

    Fix bugs

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='mdatagen',
-    version='0.0.83',
+    version='0.0.85',
     keywords=['machine learning', 'preprocessing data'],
     license='MIT',
     author='Arthur Dantas Mangussi',
@@ -12,16 +15,13 @@ setup(
     url='https://github.com/ArthurMangussi/pymdatagen',
     
     packages=find_packages(),
-    description='mdatagen: A Python library to Generate Artifical Missing Data',
-    long_description=open('README.md').read(),
+    description='mdatagen: A Python library to Generate Artificial Missing Data',
+    long_description=long_description,
     long_description_content_type='text/markdown',
 
+    python_requires='>=3.11', 
     install_requires=[
-        'python_version >= 3.11',
         'numpy >= 1.25.0',
         'pandas >= 2.0.3',
-        'scikit-learn == 1.3.0',
-    ],
+        'scikit-learn == 1.3.0']
 )
-
-
