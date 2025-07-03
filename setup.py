15f573ccc06dd6b381c738cf92c888ba1d3db7b5
Arthur Mangussi <mangussiarthur@gmail.com>
2025-07-03 16:55:55 -0300

    setup 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='mdatagen',
-    version='0.1.72',
+    version='0.2.0',
     keywords=['machine learning', 'preprocessing data'],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
